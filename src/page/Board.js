d7d082ad0472063956f4797d4694ecf02504c3d8
tuioe5679 <qodlgks123@naver.com>
2022-09-06 01:25:32 +0900

    feat: Board CSS 적용

--- src/page/Board.js
+++ src/page/Board.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Header from "./component/Header";
+import "../css/board.css"
 
 function Board(boardId) {
     const [boardItem, setBoardItem] = useState([]);
