5f957a96de8bf863e4b7a8e8f392ee16c9532421
tuioe5679 <qodlgks123@naver.com>
2022-09-09 21:10:03 +0900

    feat: 댓글 조회 기능

--- src/page/Board.js
+++ src/page/Board.js
@@ -5,10 +5,27 @@ import Header from "./component/Header";
 import { useNavigate } from 'react-router';
 import "../css/board.css"
 
+function createMarkup(element) {
+
+    return { __html: element };
+}
+
+function BoardView() {
+    let { boardId } = useParams();
+    const item = Board(boardId);
+
+    return (<>
+        <div>
+            {item}
+        </div>
+    </>)
+}
+
 function Board(boardId) {
     const navigate = useNavigate();
     const [boardItem, setBoardItem] = useState([]);
-    const [commentItem, setCommentItem] = useState({
+    const [commentItem, setCommentItem] = useState([]);
+    const [postCommentItem, setPostCommentItem] = useState({
         content: ''
     });
 
@@ -16,22 +33,29 @@ function Board(boardId) {
         if (boardItem == "") {
             Axios.get('http://localhost:8080/board/' + boardId).then((response) => {
                 setBoardItem(response.data);
+                console.log(response.data);
             }).catch(err => console.log(err))
         }
+        //else if (commentItem == "") {
+        Axios.get('http://localhost:8080/comments').then((response) => {
+            setCommentItem(response.data);
+
+        }).catch(err => console.log(err))
+        //}
     });
 
     const getValue = e => {
         const { name, value } = e.target;
 
-        setCommentItem({
-            ...commentItem,
+        setPostCommentItem({
+            ...postCommentItem,
             [name]: value
         })
     };
 
     const submitCommentPosting = () => {
         Axios.post('http://localhost:8080/comment', {
-            content: commentItem.comment
+            content: postCommentItem.comment
         }).then(() => {
 
             alert('등록 완료');
@@ -54,25 +78,21 @@ function Board(boardId) {
             <input className='comment-input' type='text' placeholder='댓글 입력'
                 onChange={getValue} name='comment' />
             <button className="comment-submit-btn" onClick={submitCommentPosting}>댓글 쓰기</button>
+            <div className="comment-item">
+                {commentItem.map(item =>
+                    <div>
+                        <div>{item.content}</div>
+                        <div>{item.nickname}</div>
+                        <div>{item.date}</div>
+                        <hr></hr>
+                    </div>
+                )}
+            </div>
         </div></>)
 
     return board;
 }
 
-function createMarkup(element) {
 
-    return { __html: element };
-}
-
-function BoardView() {
-    let { boardId } = useParams();
-    const item = Board(boardId);
-
-    return (<>
-        <div>
-            {item}
-        </div>
-    </>)
-}
 
 export default BoardView;
