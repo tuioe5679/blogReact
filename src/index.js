388465ff28456f9d0746f290761c4f65df7f7448
tuioe5679 <qodlgks123@naver.com>
2022-09-09 16:07:39 +0900

    index import 이름 수정

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route, } from "react-router-dom"
-import Main from './page/main';
+import Main from './page/Main';
 import Board from './page/Board'
-import Write from './page/write';
+import Write from './page/Write';
 import Login from "./page/Login";
 
 const rootElement = document.getElementById("root");
@@ -10,10 +10,10 @@ render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="board/:boardId" element={<Board/>} />
+      <Route path="board/:boardId" element={<Board />} />
       <Route path="write" element={<Write />} />
-      <Route path="login" element={<Login/>} />
-      <Route path="*" element={<Main />}/>
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<Main />} />
     </Routes>
   </BrowserRouter>,
   rootElement
