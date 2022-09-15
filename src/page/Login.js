fbf9a3277c1202f979bd87334fa38c2b0de5c36c
tuioe5679 <qodlgks123@naver.com>
2022-09-15 23:43:38 +0900

    feat: 로그인 기능 구현중

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,13 +1,31 @@
-import React from "react";
+import { Axios } from "axios";
+import React, { useState } from "react";
 
 function Login() {
+    const [loginData, setLoginData] = useState([]);
+
+    const login = () => {
+        Axios.post('http://localhost:8080/login', {
+            email: loginData.email,
+            password: loginData.password
+        }).then(() => {
+            alert("로그인 완료");
+        })
+    }
+
+    const getvalue = e => {
+        const { name, value } = e.target;
+
+        setLoginData({
+            ...loginData,
+            [name]: value
+        })
+    }
     return (
         <div>
-            <form action="/login" method="POST"/>
-                <input type="text" name="email" placeholder="이메일"/><br/>
-                <input type="password" name="password" placeholder="비밀번호"/><br/>
-                <input type="submit" value="Login"/><br/>
-            <form/>
+            <input type="text" name="email" placeholder="이메일" onChange={getvalue} /><br />
+            <input type="password" name="password" placeholder="비밀번호" onChange={getvalue} /><br />
+            <button className="loginsubmit" onClick={login}>로그인</button>
         </div>
     )
 }
