c60cf6b4ff6a788fc640377debf8e84cabc0a424
tuioe5679 <qodlgks123@naver.com>
2022-09-15 23:45:41 +0900

    refactor: header 컴포넌트 CSS 분리

--- src/page/component/Header.js
+++ src/page/component/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
+import '../../css/header.css';
 
 function Header(){
     return (
         <div className='blog-nav'>
                 <div className='title'><a href='/'>blog</a></div>
                 <ul className='menu'>
-                    <li><a href='/login'>로그인</a></li>
-                    <li><a href='/write'>글쓰기</a></li>
+                    <li className="administrator"><a href='/admin'>관리자</a></li>
+                    <li className="login"><a href='/login'>로그인</a></li>
+                    <li className="write"><a href='/write'>글쓰기</a></li>
                 </ul>
             </div>
     )
