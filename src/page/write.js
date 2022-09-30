823f3c48d925b9084e3b44462fb3c6e6ee6d6e4a
tuioe5679 <qodlgks123@naver.com>
2022-10-01 00:43:54 +0900

    Write 페이지 수정

--- src/page/write.js
+++ src/page/write.js
@@ -7,8 +7,48 @@ import Header from "./component/Header"
 import Axios from 'axios';
 import '../css/write.css';
 
+const API_URL = "https://77em4-8080.sse.codesandbox.io";
+const UPLOAD_ENDPOINT = "upload_files";
+
+
 
 function Write() {
+
+
+    function uploadAdapter(loader) {
+        return {
+            upload: () => {
+                return new Promise((resolve, reject) => {
+                    const body = new FormData();
+                    loader.file.then((file) => {
+                        body.append("files", file);
+                        // let headers = new Headers();
+                        // headers.append("Origin", "http://localhost:3000");
+                        fetch(`${API_URL}/${UPLOAD_ENDPOINT}`, {
+                            method: "post",
+                            body: body
+                            // mode: "no-cors"
+                        })
+                            .then((res) => res.json())
+                            .then((res) => {
+                                resolve({
+                                    default: `${API_URL}/${res.filename}`
+                                });
+                            })
+                            .catch((err) => {
+                                reject(err);
+                            });
+                    });
+                });
+            }
+        };
+    }
+    function uploadPlugin(editor) {
+        editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+            return uploadAdapter(loader);
+        };
+    }
+
     const navigate = useNavigate();
 
     const [Board, setBoard] = useState({
@@ -45,6 +85,9 @@ function Write() {
                 </div>
                 <CKEditor
                     editor={ClassicEditor}
+                    config={{
+                        extraPlugins: [uploadPlugin]
+                    }}
                     data=""
                     onReady={editor => {
                     }}
